8c696073fca2d835a1cf4fff101981e09c0ea71a
MemoSan27 <135660314+MemoSan27@users.noreply.github.com>
2024-01-24 19:06:10 -0700

    image model working

--- src/routes/image.router.js
+++ src/routes/image.router.js
@@ -1,9 +1,15 @@
-const { getAll } = require('../controllers/image.controller');
+const { getAll, create, remove } = require('../controllers/image.controller');
 const express = require('express');
+const upload = require('../utils/multer');
+const verifyJWT = require('../utils/verifyJWT');
 
 const imageRouter = express.Router();
 
 imageRouter.route('/')
-    .get(getAll)
+    .get(verifyJWT, getAll)
+    .post(verifyJWT, upload.single('image'), create)
+
+imageRouter.route('/:id')
+    .delete(verifyJWT, remove)
 
 module.exports = imageRouter;
